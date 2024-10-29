be035311af960d943ee07bb4e0c396ba6a87e7e5
MaliQoc <alihasanozankoc@gmail.com>
2024-10-29 04:33:52 +0300

    first commit

---
+++ models/invoiceModel.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Invoice struct {
+	ID               primitive.ObjectID `bson:"_id"`
+	Invoice_id       string             `json:"invoice_id"`
+	Order_id         string             `json:"order_id"`
+	Payment_method   *string            `json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
+	Payment_status   *string            `json:"payment_status" validate:"required,eq=PENDING|eq=PAID"`
+	Payment_due_date *string            `json:"payment_due_date"`
+	Created_at       *string            `json:"created_at"`
+	Updated_at       *string            `json:"updated_at"`
+}
